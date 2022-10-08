308cbf155adac256bfa3c59604f515413432d63b
Itsjustblack <jason.aghedo@gmail.com>
2022-10-08 20:04:14 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,62 +1,69 @@
-import { useEffect, useState } from 'react';
-import BarChart from './components/BarChart';
-import logo from './images/logo.svg';
+import { useEffect, useState } from "react";
+import BarChart from "./components/BarChart";
+import logo from "./images/logo.svg";
 import values from "./data.json";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  const { Expenses } = values;
-  const [days, setDays] = useState(null);
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [total, setTotal] = useState(0);
+	const { Expenses } = values;
+	const [days, setDays] = useState(null);
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [total, setTotal] = useState(0);
 
-  useEffect(() => {
-    setDays(Expenses.map(item => item.day));
-    setData(Expenses.map(item => item.amount));
-    setIsPending(false);
-  }, []);
-  
-  useEffect(() => {
-    data ? setTotal(data.reduce((a,b) => a + b)) : setTotal(0)
-  }, [data]);
+	useEffect(() => {
+		setDays(Expenses.map((item) => item.day));
+		setData(Expenses.map((item) => item.amount));
+		setIsPending(false);
+	}, []);
 
+	useEffect(() => {
+		data ? setTotal(data.reduce((a, b) => a + b)) : setTotal(0);
+	}, [data]);
 
-  
-  return(
-    <div className="app">
-      <div className="container">
-        <header>
-          <div className="title">
-            <span>My Balance</span>
-            <h1 className="total">{"$"+total}</h1>
-          </div>
-            <img src={logo} alt="Logo" />
-        </header>
-        <main>
-          <h2>Spending - Last 7 Days</h2>
-            {/* {isPending && <div>...Loading...</div>} Change to Loading Circle */}
-            {<BarChart days={days}  data={data}/>}  
-          <div className="line"></div>
-          <div className="footer">
-            <div className='month'>
-              <p>Total this Month</p>
-              <h3>{"$" + total}</h3>
-            </div>
-            <div className="lastmonth">
-              <h4>+2.4%</h4>
-              <p>from last month</p>
-            </div>
-          </div>
-        </main>
-        <div className="attribution">
-        Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge"
-          >Frontend Mentor</a
-        >. Coded by <a href="#">Your Name Here</a>.
-      </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="app">
+			<div className="container">
+				<header>
+					<div className="title">
+						<span>My Balance</span>
+						<h1 className="total">{"$" + total}</h1>
+					</div>
+					<img
+						src={logo}
+						alt="Logo"
+					/>
+				</header>
+				<main>
+					<h2>Spending - Last 7 Days</h2>
+					{/* {isPending && <div>...Loading...</div>} Change to Loading Circle */}
+					{
+						<BarChart
+							days={days}
+							data={data}
+						/>
+					}
+					<div className="line"></div>
+					<div className="footer">
+						<div className="month">
+							<p>Total this Month</p>
+							<h3>{"$" + total}</h3>
+						</div>
+						<div className="lastmonth">
+							<h4>+2.4%</h4>
+							<p>from last month</p>
+						</div>
+					</div>
+				</main>
+				<div className="attribution">
+					Challenge by
+					<a href="https://www.frontendmentor.io?ref=challenge">
+						Frontend Mentor
+					</a>
+					. Coded by <a href="#">Jason Aghedo</a>.
+				</div>
+			</div>
+		</div>
+	);
+};
 export default App;
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,76 +1,77 @@
 import React, { useEffect } from "react";
-import Chart from 'chart.js/auto';
-import { Bar } from "react-chartjs-2"
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 // import { faker } from '@faker-js/faker';
 
-const options = { // Dont allow color change when Click on bars
-  responsive: true,
-  scales: {
-    x: {
-      grid: {
-        display: false,
-      },
-      ticks: {
-        display: true,
-      }
-    },
-    y: {
-      beginAtZero: true,
-        display: false
-    }
-  },
-  plugins:{
-    tooltip: {
-      // callbacks: {
-      //   title : () => null,
-      //   beforeBody: () => '$'
-      // },
-      bodyAlign: 'left',
-      boxWidth: 10,
-      backgroundColor: 'rgb(56, 35, 20)',
-      displayColors: false,
-    },
-    legend: {
-      display: false,
-    }
-  }
+const options = {
+	// Dont allow color change when Click on bars
+	responsive: true,
+	scales: {
+		x: {
+			grid: {
+				display: false,
+			},
+			ticks: {
+				display: true,
+			},
+		},
+		y: {
+			beginAtZero: true,
+			display: false,
+		},
+	},
+	plugins: {
+		tooltip: {
+			// callbacks: {
+			//   title : () => null,
+			//   beforeBody: () => '$'
+			// },
+			bodyAlign: "left",
+			boxWidth: 10,
+			backgroundColor: "rgb(56, 35, 20)",
+			displayColors: false,
+		},
+		legend: {
+			display: false,
+		},
+	},
 };
-  const bgColor = [
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-    "rgba(236, 119, 95)",
-  ];
+const bgColor = [
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+	"rgba(236, 119, 95)",
+];
 
-const BarChart = ({days, data: amounts}) => {
-  const data = {
-    labels: days,
-    datasets: [
-  {
-    data: amounts,
-    backgroundColor: bgColor,
-    borderRadius: 3,
-  }
-],
+const BarChart = ({ days, data: amounts }) => {
+	const data = {
+		labels: days,
+		datasets: [
+			{
+				data: amounts,
+				backgroundColor: bgColor,
+				borderRadius: 3,
+			},
+		],
+	};
+
+	useEffect(() => {
+		if (amounts) {
+			const max = amounts.indexOf(Math.max(...amounts));
+			bgColor[max] = "hsl(186, 34%, 60%)";
+			// console.log(bgColor);
+		}
+	}, [amounts]);
+	return (
+		<Bar
+			height={220}
+			data={data}
+			options={options}
+		/>
+	);
 };
-
-  useEffect(() => {
-    if(amounts){
-      const max = amounts.indexOf(Math.max(...amounts));
-      bgColor[max] = "hsl(186, 34%, 60%)";
-      // console.log(bgColor);
-    }
-  }, [amounts]);
-    return(
-            <Bar
-            height={220}
-            data={data}
-            options={options}
-            />
-    )
-}
 
 export default BarChart;
